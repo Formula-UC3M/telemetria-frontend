a82c92f311027a2f5cd05b2c4cdff4ff4995ca4b
Alfredo De la calle <alfredodelacalle@MacBook-Pro-de-Alfredo.local>
2018-05-08 22:56:18 +0200

    [Add] Vue MQTT client

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import VueMqtt from 'vue-mqtt';
+Vue.use(VueMqtt, 'http://localhost:3001', {});
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  mqtt: {
+    'formula/+' (data, topic) {
+      console.log(data.toString(), topic);
+    }
+  },
+  mounted() {
+    // this.$mqtt.subscribe('formula/#');
+  },
   render: h => h(App),
 }).$mount('#app');
