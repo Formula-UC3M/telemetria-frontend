0b8fa9cedd2a0f803d2ded2dd255987b79fcca9c
Alfredo de la Calle <alfredo.delacalle@gmail.com>
2018-06-15 18:09:48 +0200

    ip

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import auth from './auth';
 import ranges from './ranges';
 
-export const API_URL = 'http://localhost:3001';
+export const API_URL = 'http://192.168.1.202:3001';
 
 export default {
   auth,
