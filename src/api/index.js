55c50fdb0ff8299312a2443b05300b094699111a
Sebastián Cabanas <sebastian.cabanas@cbiconsulting.es>
2019-04-18 04:34:02 +0200

    Set development server url and user telemetria-backend.herokuapp.com (web and mqqt). Rebuild.

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import auth from './auth'
 import ranges from './ranges'
 
-export const API_URL = 'http://localhost:3001'
-export const WS_URL = 'ws://localhost:3001'
+export const API_URL = 'https://telemetria-backend.herokuapp.com'
+export const WS_URL = 'ws://telemetria-backend.herokuapp.com'
 
 export default {
   auth,
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
+import VueMqtt from 'vue-mqtt'
+
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import { WS_URL } from './api/index'
 
-import { API_URL, WS_URL } from './api/index'
-
-import VueMqtt from 'vue-mqtt'
-// alert(API_URL)
 Vue.use(VueMqtt, WS_URL, {})
 
 Vue.config.productionTip = false
@@ -15,11 +14,10 @@ new Vue({
   router,
   store,
   mounted () {
-    // alert('mounted')
     this.$store.dispatch('fetchConfig')
     this.$store.dispatch('login', {
-      email: '1234@gmail.com',
-      password: '1234'
+      email: 'test@telemetria.com',
+      password: 'abc123..'
     })
     this.$mqtt.subscribe('formula-fake-data/+')
     this.$mqtt.subscribe('formula-fake-data/ecu/+')
