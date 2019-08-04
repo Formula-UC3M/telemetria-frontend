d43fcdc2bf075277e0047a5cb7ab4440bc0d15dd
Sebastián Cabanas <sebastian.cabanas@cbiconsulting.es>
2019-08-04 21:11:30 +0200

    Decoding server data in the right way.

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,8 +1,6 @@
-export const convertU8 = (data) => {
-    if (data instanceof Uint8Array) {
-        return data[0];
-    }
+const decoder = new TextDecoder("utf-8");
 
-    return Buffer.from(data).readUIntBE(0, data.length);
+export const convertU8 = (data) => {
+    return parseInt(decoder.decode(data));
 }
   
